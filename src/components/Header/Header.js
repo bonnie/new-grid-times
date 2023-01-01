5986d7d03299dcd48c0a897921d5bc918328ce9a
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-12-31 18:43:07 -0800

    Update header for responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,40 +8,73 @@ import Button from "../Button";
 
 const Header = () => {
   return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
+    <Wrapper>
+      <Side>
+        <SuperHeader>
+          <Row>
+            <ActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+            </ActionGroup>
+            <UserButton>
+              <button>
+                <User size={24} />
+              </button>
+            </UserButton>
+          </Row>
+        </SuperHeader>
+      </Side>
       <MainHeader>
         <Logo />
       </MainHeader>
-    </header>
+      <Side>
+        <SubscribeLogin>
+          <Button>Subscribe</Button>
+          <LoginLink>Already a subscriber?</LoginLink>
+        </SubscribeLogin>
+      </Side>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`
+  @media ${(props) => props.theme.queries.laptopAndUp} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Side = styled.div`
+  @media ${(props) => props.theme.queries.laptopAndUp} {
+    flex: 1;
+    max-width: 160px;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: ${(props) => props.theme.colors.gray[900]};
   color: white;
+
+  @media ${(props) => props.theme.queries.laptopAndUp} {
+    color: ${(props) => props.theme.colors.gray[900]};
+    background: transparent;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+
+  @media ${(props) => props.theme.queries.laptopAndUp} {
+    position: absolute;
+    top: 55px;
+  }
 `;
 
 const ActionGroup = styled.div`
@@ -57,12 +90,35 @@ const ActionGroup = styled.div`
   }
 `;
 
+const UserButton = styled(ActionGroup)`
+  @media ${(props) => props.theme.queries.laptopAndUp} {
+    display: none;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+`;
+
+const SubscribeLogin = styled.div`
+  display: none;
+
+  @media ${(props) => props.theme.queries.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    margin-right: 32px;
+  }
+`;
+
+const LoginLink = styled.a`
+  font-style: italic;
+  text-decoration: underline;
+  font-size: calc(14 / 16) rem;
+  margin-top: 8px;
 `;
 
 export default Header;
